724ad10429d47ed26df2563f9a0581234cb1f6a2
CristhianCV <cristhiancv45@gmail.com>
2017-08-28 15:02:35 -0500

    Carga de Datos

--- ProjectLion/ProjectLion.Android/SplashActivity.cs
+++ ProjectLion/ProjectLion.Android/SplashActivity.cs
@@ -1,5 +1,8 @@
 ﻿using Android.App;
 using Android.OS;
+using Android.Widget;
+using Java.Lang;
+using System.Threading;
 
 namespace ProjectLion.Droid
 {
@@ -11,8 +14,22 @@ namespace ProjectLion.Droid
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
-            System.Threading.Thread.Sleep(30);
-            StartActivity(typeof(MainActivity));
+
+            //System.Threading.Thread.Sleep(30);
+
+            ProgressDialog dialog = new ProgressDialog(this); // this = YourActivity
+            dialog.SetProgressStyle(ProgressDialogStyle.Spinner);
+            dialog.SetMessage("Cargando datos");
+            dialog.SetCanceledOnTouchOutside(false);
+
+            dialog.Show();
+
+            Runnable progressRunnable = new Runnable(delegate { StartActivity(typeof(MainActivity)); });
+
+            Handler pdCanceller = new Handler();
+            pdCanceller.PostDelayed(progressRunnable, 6000);
+                       
         }
+
     }
 }
